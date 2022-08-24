b5ab4946b0fb7cd631c9b1e5e88081cffeec8aed
Mob <tammanoon24@gmail.com>
2022-08-24 16:41:08 +0700

    :lipstick: Init tailwindcss

--- dapp/src/App.tsx
+++ dapp/src/App.tsx
@@ -18,6 +18,9 @@ function App() {
         >
           Learn React
         </a>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
       </header>
     </div>
   );
