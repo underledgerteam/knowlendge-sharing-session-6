3715e852eb3df5ed390043393301126b804a521c
Mob <tammanoon24@gmail.com>
2022-09-15 22:32:09 +0700

    :lipstick: Update navbar

--- dapp/src/components/Navbar.tsx
+++ dapp/src/components/Navbar.tsx
@@ -1,11 +1,27 @@
 import { FC, useContext } from "react";
+import { Link, useLocation } from "react-router-dom";
 import { Web3Context } from "src/contexts/web3.context";
+
+const navData = [
+  {
+    name: "Bank",
+    href: "/bank",
+  },
+  {
+    name: "Token",
+    href: "/token",
+  },
+  {
+    name: "NFT",
+    href: "/nft",
+  }
+];
 
 const ConnectButton = () => {
   const { walletAddress, handleConnectWallet } = useContext(Web3Context);
   if (walletAddress) {
     return (
-      <div className="text-2xl text-white">
+      <div className="text-2xl text-white font-bold">
         Connected
       </div>
     );
@@ -22,23 +38,34 @@ const ConnectButton = () => {
   );
 };
 
+const NavMenu = () => {
+  const { pathname } = useLocation();
+  return (
+    <div className="flex pl-8 list-style-none mr-auto">
+      {navData.map(item => {
+        const { name, href } = item;
+        const isActive = (pathname === href) ? "font-bold underline" : "font-normal";
+        return (
+          <Link key={name} to={href} className="p-6">
+            <div className={`text-2xl ${isActive} text-white hover:text-gray-700 focus:text-gray-700`}>
+              {name}
+            </div>
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
 const MyNavbar: FC = () => {
   return (
     <nav className="w-full h-[10vh] flex justify-between items-center p-4 bg-slate-900">
-      <a className="flex-initial justify-center items-center font-bold text-4xl text-white" href="/">
-        Workshop
-      </a>
-      <ul className="flex pl-8 list-style-none mr-auto">
-        <li className="nav-item p-6">
-          <a className="text-2xl text-white hover:text-gray-700 focus:text-gray-700" href="bank">Bank</a>
-        </li>
-        <li className="nav-item p-6">
-          <a className="text-2xl text-white hover:text-gray-700 focus:text-gray-700" href="token">Token</a>
-        </li>
-        <li className="nav-item p-6">
-          <a className="text-2xl text-white hover:text-gray-700 focus:text-gray-700" href="nft">NFT</a>
-        </li>
-      </ul>
+      <Link key="home" to="/">
+        <div className="flex-initial justify-center items-center font-extrabold text-4xl text-white">
+          Workshop
+        </div>
+      </Link>
+      <NavMenu />
       <ConnectButton />
     </nav>
   );
