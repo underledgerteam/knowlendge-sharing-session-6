c507a40ed6edd4a167868152a3918a2567db2d33
Mob <tammanoon24@gmail.com>
2022-09-19 19:00:30 +0700

    :sparkles: Prepare nft page
    comment code for workshop

--- dapp/src/pages/NFT.tsx
+++ dapp/src/pages/NFT.tsx
@@ -20,23 +20,23 @@ const { ethereum } = window;
 
 const NFT: FC = () => {
   // #1
-  const { walletAddress, walletBalance, getWalletBalance } = useContext(Web3Context);
-  const [cost, setCost] = useState("");
-  const [mintQty, setMintQty] = useState("");
-  const [maxMintQty, setMaxMintQty] = useState("");
-  const [totalSupply, setTotalSupply] = useState("");
-  const [maxSupply, setMaxSupply] = useState("");
-  const [nftList, setNftList] = useState<NftImageInterface[]>([]);
+  // const { walletAddress, walletBalance, getWalletBalance } = useContext(Web3Context);
+  // const [cost, setCost] = useState("");
+  // const [mintQty, setMintQty] = useState("");
+  // const [maxMintQty, setMaxMintQty] = useState("");
+  // const [totalSupply, setTotalSupply] = useState("");
+  // const [maxSupply, setMaxSupply] = useState("");
+  // const [nftList, setNftList] = useState<NftImageInterface[]>([]);
 
   // #1
-  const initContract = () => {
-    const provider = new ethers.providers.Web3Provider(ethereum);
-    const signer = provider.getSigner();
-    const nftContract = new ethers.Contract(NFT_ADDRESS, NFT_ABI, signer);
-    return nftContract;
-  };
+  // const initContract = () => {
+  //   const provider = new ethers.providers.Web3Provider(ethereum);
+  //   const signer = provider.getSigner();
+  //   const nftContract = new ethers.Contract(NFT_ADDRESS, NFT_ABI, signer);
+  //   return nftContract;
+  // };
 
-  // #2
+  // #2 
   /* 
     Ask about ipfs 
     We could use IPFS, a decentralized protocol and peer-to-peer network 
@@ -44,133 +44,134 @@ const NFT: FC = () => {
     As this protocol is DECENTRALIZED and FREE, it is our best option!
     ref : alchemy doc
   */
-  const getCost = async () => {
-    const contract = initContract();
-    const result = await contract.cost();
-    // console.log('getCost', ethers.utils.formatEther(result));
-    setCost(ethers.utils.formatEther(result));
-  };
-  const getMaxMintAmount = async () => {
-    const contract = initContract();
-    const result = await contract.maxMintAmount();
-    // console.log('getMaxMintAmount', ethers.utils.formatUnits(result, 'wei'));
-    setMaxMintQty(ethers.utils.formatUnits(result, 'wei'));
-  };
-  const getTotalSupply = async () => {
-    const contract = initContract();
-    const result = await contract.totalSupply();
-    // console.log('getTotalSupply', ethers.utils.formatUnits(result, 'wei'));
-    setTotalSupply(ethers.utils.formatUnits(result, 'wei'));
-  };
-  const getMaxSupply = async () => {
-    const contract = initContract();
-    const result = await contract.maxSupply();
-    // console.log('getMaxSupply', ethers.utils.formatUnits(result, 'wei'));
-    setMaxSupply(ethers.utils.formatUnits(result, 'wei'));
-  };
-  // start nft section
-  const getTokenUri = async (tokenId: number) => {
-    const contract = initContract();
-    const tokenUri = await contract.tokenURI(tokenId);
-    // console.log('getTokenUri', tokenUri);
-    return replaceUriToHttps(tokenUri);
-  };
-  const getNftMetaData = async (nftUrl: any) => {
-    const result = await fetch(nftUrl);
-    const nftMetaData = await result.json();
-    return {
-      ...nftMetaData,
-      imageUrl: replaceUriToHttps(nftMetaData.image),
-      date: dayjs(nftMetaData.date).format("DD/MM/YYYY"),
-    };
-  };
-  const getNftListByAddress = async () => {
-    const contract = initContract();
-    const nftIdArray = await contract.walletOfOwner(walletAddress);
-    // console.log('getNftListByAddress', nftIdArray);
-    return nftIdArray;
-  };
-  const replaceUriToHttps = (uri: string) => {
-    return uri.replace('ipfs://', 'https://ipfs.io/ipfs/');
-  };
-  const handleGetNftList = async () => {
-    let tokenUriPromises = [];
-    let tokenMetaDataPromises = [];
+  // const getCost = async () => {
+  //   const contract = initContract();
+  //   const result = await contract.cost();
+  //   // console.log('getCost', ethers.utils.formatEther(result));
+  //   setCost(ethers.utils.formatEther(result));
+  // };
+  // const getMaxMintAmount = async () => {
+  //   const contract = initContract();
+  //   const result = await contract.maxMintAmount();
+  //   // console.log('getMaxMintAmount', ethers.utils.formatUnits(result, 'wei'));
+  //   setMaxMintQty(ethers.utils.formatUnits(result, 'wei'));
+  // };
+  // const getTotalSupply = async () => {
+  //   const contract = initContract();
+  //   const result = await contract.totalSupply();
+  //   // console.log('getTotalSupply', ethers.utils.formatUnits(result, 'wei'));
+  //   setTotalSupply(ethers.utils.formatUnits(result, 'wei'));
+  // };
+  // const getMaxSupply = async () => {
+  //   const contract = initContract();
+  //   const result = await contract.maxSupply();
+  //   // console.log('getMaxSupply', ethers.utils.formatUnits(result, 'wei'));
+  //   setMaxSupply(ethers.utils.formatUnits(result, 'wei'));
+  // };
+  // // start nft section
+  // const getTokenUri = async (tokenId: number) => {
+  //   const contract = initContract();
+  //   const tokenUri = await contract.tokenURI(tokenId);
+  //   // console.log('getTokenUri', tokenUri);
+  //   return replaceUriToHttps(tokenUri);
+  // };
+  // const getNftMetaData = async (nftUrl: any) => {
+  //   const result = await fetch(nftUrl);
+  //   const nftMetaData = await result.json();
+  //   return {
+  //     ...nftMetaData,
+  //     imageUrl: replaceUriToHttps(nftMetaData.image),
+  //     date: dayjs(nftMetaData.date).format("DD/MM/YYYY"),
+  //   };
+  // };
+  // const getNftListByAddress = async () => {
+  //   const contract = initContract();
+  //   const nftIdArray = await contract.walletOfOwner(walletAddress);
+  //   // console.log('getNftListByAddress', nftIdArray);
+  //   return nftIdArray;
+  // };
+  // const replaceUriToHttps = (uri: string) => {
+  //   return uri.replace('ipfs://', 'https://ipfs.io/ipfs/');
+  // };
+  // const handleGetNftList = async () => {
+  //   let tokenUriPromises = [];
+  //   let tokenMetaDataPromises = [];
 
-    // 1st get all nft id
-    const nftIdArray = await getNftListByAddress();
+  //   // 1st get all nft id
+  //   const nftIdArray = await getNftListByAddress();
 
-    // 2nd
-    // prepare id for using getTokenUri()
-    for (let id of nftIdArray) {
-      tokenUriPromises.push(getTokenUri(id));
-    }
-    // parallel get all token uri 
-    const tokenUriArray = await Promise.all(tokenUriPromises);
-    // console.log('tokenUriArray', tokenUriArray);
+  //   // 2nd
+  //   // prepare id for using getTokenUri()
+  //   for (let id of nftIdArray) {
+  //     tokenUriPromises.push(getTokenUri(id));
+  //   }
+  //   // parallel get all token uri 
+  //   const tokenUriArray = await Promise.all(tokenUriPromises);
+  //   // console.log('tokenUriArray', tokenUriArray);
 
-    // 3rd
-    // prepare data for using getNftMetaData()
-    for (let tokenUri of tokenUriArray) {
-      tokenMetaDataPromises.push(getNftMetaData(tokenUri));
-    }
-    // parallel get all token meta data
-    const tokenMetaDataArray = await Promise.all(tokenMetaDataPromises);
-    console.log('tokenMetaDataArray', tokenMetaDataArray);
-    setNftList(tokenMetaDataArray);
-  };
-  // end nft section
+  //   // 3rd
+  //   // prepare data for using getNftMetaData()
+  //   for (let tokenUri of tokenUriArray) {
+  //     tokenMetaDataPromises.push(getNftMetaData(tokenUri));
+  //   }
+  //   // parallel get all token meta data
+  //   const tokenMetaDataArray = await Promise.all(tokenMetaDataPromises);
+  //   console.log('tokenMetaDataArray', tokenMetaDataArray);
+  //   setNftList(tokenMetaDataArray);
+  // };
+  // end #2 nft section
 
   // #4 function mint nft
   // show hack
-  const handleMint = async () => {
-    if (parseInt(mintQty) > parseInt(maxMintQty)) {
-      alert(`Max mint per mint is ${maxMintQty}`);
-      return true;
-    }
-    const mintPrice = (parseFloat(cost) * parseInt(mintQty)).toString();
-    const tx = {
-      value: ethers.utils.parseEther(mintPrice)
-    };
-    const contract = initContract();
-    const result = await contract.mint(mintQty, tx);
-    console.log('handleMint', result);
-    // case hack
-    // const contract = initContract();
-    // const result = await contract.mint(mintQty);
-  };
+  // const handleMint = async () => {
+  //   if (parseInt(mintQty) > parseInt(maxMintQty)) {
+  //     alert(`Max mint per mint is ${maxMintQty}`);
+  //     return true;
+  //   }
+  //   const mintPrice = (parseFloat(cost) * parseInt(mintQty)).toString();
+  //   const tx = {
+  //     value: ethers.utils.parseEther(mintPrice)
+  //   };
+  //   const contract = initContract();
+  //   const result = await contract.mint(mintQty, tx);
+  //   console.log('handleMint', result);
+  //   // case hack
+  //   // const contract = initContract();
+  //   // const result = await contract.mint(mintQty);
+  // };
 
   // #3 
-  const handleRefresh = async () => {
-    await Promise.all([
-      getWalletBalance(),
-      getMaxSupply(),
-      handleGetNftList(),
-    ]);
-  };
+  // const handleRefresh = async () => {
+  //   await Promise.all([
+  //     getWalletBalance(),
+  //     getMaxSupply(),
+  //     handleGetNftList(),
+  //   ]);
+  // };
 
   // #2
-  useEffect(() => {
-    const init = async () => {
-      if (walletAddress) {
-        await Promise.all([
-          getCost(),
-          getMaxMintAmount(),
-          getTotalSupply(),
-          getMaxSupply(),
-          handleGetNftList(),
-        ]);
-      }
-    };
-    init();
-  }, [walletAddress]);
+  // useEffect(() => {
+  //   const init = async () => {
+  //     if (walletAddress) {
+  //       await Promise.all([
+  //         getCost(),
+  //         getMaxMintAmount(),
+  //         getTotalSupply(),
+  //         getMaxSupply(),
+  //         handleGetNftList(),
+  //       ]);
+  //     }
+  //   };
+  //   init();
+  // }, [walletAddress]);
 
   return (
     <div className="h-full p-4 text-center bg-slate-700">
 
       {/* #3 */}
       <div className="w-10/12 md:w-9/12 mx-auto my-4 p-4 border border-white bg-gray-100 text-2xl">
-        <div>
+        <p className="text-2xl">info</p>
+        {/* <div>
           address : {`${walletAddress || `...`}`}
         </div>
         <div>
@@ -182,12 +183,15 @@ const NFT: FC = () => {
         <div>
           Minted NFT {`${totalSupply} / ${maxSupply}`}
         </div>
-        <MyButton id="refreshButton" name="refreshButton" className="mt-4" text="REFRESH" onClick={handleRefresh} />
+        <MyButton id="refreshButton" name="refreshButton" className="mt-4" text="REFRESH" onClick={handleRefresh} /> */}
       </div>
 
       {/* #4 */}
       <div className="w-10/12 md:w-9/12 mx-auto my-4 p-4 border border-white bg-gray-100 text-2xl">
         <div>
+          Mint NFT
+        </div>
+        {/* <div>
           Max mint {`${maxMintQty}`}
         </div>
         <input
@@ -198,7 +202,7 @@ const NFT: FC = () => {
           placeholder="input mint quantity"
           onChange={e => setMintQty(e.target.value)}
         />
-        <MyButton id="mintButton" name="mintButton" className="mx-2" text="MINT" onClick={handleMint} />
+        <MyButton id="mintButton" name="mintButton" className="mx-2" text="MINT" onClick={handleMint} /> */}
       </div>
 
       {/* #3 */}
@@ -206,7 +210,7 @@ const NFT: FC = () => {
         <div>
           NFT image
         </div>
-        <div className="mt-4 grid grid-cols-4 gap-4 justify-items-center content-around">
+        {/* <div className="mt-4 grid grid-cols-4 gap-4 justify-items-center content-around">
           {nftList.map((nft) => (
             <NftImage
               key={nft.name}
@@ -218,7 +222,7 @@ const NFT: FC = () => {
               date={nft.date}
             />
           ))}
-        </div>
+        </div> */}
       </div>
 
     </div>
