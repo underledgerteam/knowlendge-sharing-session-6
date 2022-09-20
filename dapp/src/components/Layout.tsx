d6158eff0021eca0f2308d0bbb17ad6d2bc6c6f0
Mob <tammanoon24@gmail.com>
2022-09-20 13:07:21 +0700

    :sparkles: Check b4 render content

--- dapp/src/components/Layout.tsx
+++ dapp/src/components/Layout.tsx
@@ -1,12 +1,39 @@
-import { FC } from 'react';
+import { FC, useContext } from 'react';
 import { Outlet } from "react-router-dom";
+import { Web3Context } from "src/contexts/web3.context";
 import Navbar from 'src/components/Navbar';
 
 const Layout: FC = () => {
+  const { walletAddress, supportChain } = useContext(Web3Context);
+
+  const Content = () => {
+    if (!walletAddress) {
+      return (
+        <div className="h-[90vh] p-4 text-center bg-slate-700">
+          <div className="w-10/12 md:w-9/12 mx-auto my-4 p-4 border border-white bg-gray-100 text-2xl">
+            Please connect wallet
+          </div>
+        </div>
+      );
+    }
+    if (!supportChain) {
+      return (
+        <div className="h-[90vh] p-4 text-center bg-slate-700">
+          <div className="w-10/12 md:w-9/12 mx-auto my-4 p-4 border border-white bg-gray-100 text-2xl">
+            Please change to Goerli Test Network
+          </div>
+        </div>
+      );
+    }
+    return (
+      <Outlet />
+    );
+  }
+
   return (
     <div className="min-h-screen min-w-screen bg-slate-700">
       <Navbar />
-      <Outlet />
+      <Content />
     </div>
   );
 };
