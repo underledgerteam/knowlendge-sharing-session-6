27cc1fa5db67cf5fadf46255390595f2b8adf294
Mob <tammanoon24@gmail.com>
2022-09-28 19:09:56 +0700

    :rewind: Prepare code workshop

--- dapp/src/pages/Bank.tsx
+++ dapp/src/pages/Bank.tsx
@@ -9,37 +9,37 @@ const { ethereum } = window;
 
 const Bank: FC = () => {
   // #1 initial variable
-  const { walletAddress, walletBalance, getWalletBalance } = useContext(Web3Context);
-  const [balance, setBalance] = useState("");
-  const [deposit, setDeposit] = useState("");
+  // const { walletAddress, walletBalance, getWalletBalance } = useContext(Web3Context);
+  // const [balance, setBalance] = useState("");
+  // const [deposit, setDeposit] = useState("");
 
   /* 
     #1 function for prepare Contract object for use later.
     If you are familiar with Databases, this is similar to an Object Relational Mapper(ORM).
   */
-  const initContract = () => {
-    const provider = new ethers.providers.Web3Provider(ethereum);
-    const signer = provider.getSigner();
-    const simpleBankContract = new ethers.Contract(SIMPLE_BANK_ADRESS, SIMPLE_BANK_ABI, signer);
-    return simpleBankContract;
-  };
+  // const initContract = () => {
+  //   const provider = new ethers.providers.Web3Provider(ethereum);
+  //   const signer = provider.getSigner();
+  //   const simpleBankContract = new ethers.Contract(SIMPLE_BANK_ADRESS, SIMPLE_BANK_ABI, signer);
+  //   return simpleBankContract;
+  // };
 
   // #2 function to get balance in contract.
-  const getBalance = async (): Promise<void> => {
-    const contract = initContract();
-    let result = await contract.balances(walletAddress);
-    console.log('getBalance', result);
-    console.log('getBalance', ethers.utils.formatUnits(result));
-    setBalance(ethers.utils.formatUnits(result));
-  };
+  // const getBalance = async (): Promise<void> => {
+  //   const contract = initContract();
+  //   let result = await contract.balances(walletAddress);
+  //   console.log('getBalance', result);
+  //   console.log('getBalance', ethers.utils.formatUnits(result));
+  //   setBalance(ethers.utils.formatUnits(result));
+  // };
 
   // #4 function to get balance in contract and wallet balance.
-  const refreshBalance = async () => {
-    await Promise.all([
-      getWalletBalance(),
-      getBalance()
-    ]);
-  };
+  // const refreshBalance = async () => {
+  //   await Promise.all([
+  //     getWalletBalance(),
+  //     getBalance()
+  //   ]);
+  // };
 
   // #auto refresh
   // const waitForTx = async (txHash: string) => {
@@ -50,52 +50,52 @@ const Bank: FC = () => {
   // };
 
   // #5 handle change input value 
-  const handleChangeDeposit = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setDeposit(e.target.value);
-  };
+  // const handleChangeDeposit = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   setDeposit(e.target.value);
+  // };
 
   // #5 using deposit
   // After this function tx is created but not mean it mined.
   // have to wait tx mined then this tx will write to blockchain.
   // then use refreshBalance() to get current value
-  const handleDeposit = async (): Promise<void> => {
-    try {
-      const tx = {
-        value: ethers.utils.parseEther(deposit)
-      };
-      const contract = initContract();
-      const result = await contract.deposit({ ...tx });
-      console.log('handleDeposit', result);
-      // await waitForTx(result.hash);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  // const handleDeposit = async (): Promise<void> => {
+  //   try {
+  //     const tx = {
+  //       value: ethers.utils.parseEther(deposit)
+  //     };
+  //     const contract = initContract();
+  //     const result = await contract.deposit({ ...tx });
+  //     console.log('handleDeposit', result);
+  //     // await waitForTx(result.hash);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
   // #6 using withdraw
-  const handleWithdraw = async (): Promise<void> => {
-    try {
-      const contract = initContract();
-      const tx = {
-        gasLimit: 3000000
-      };
-      const result = await contract.withdraw({ ...tx });
-      console.log('handleWithdraw', result);
-      // await waitForTx(result.hash);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  // const handleWithdraw = async (): Promise<void> => {
+  //   try {
+  //     const contract = initContract();
+  //     const tx = {
+  //       gasLimit: 3000000
+  //     };
+  //     const result = await contract.withdraw({ ...tx });
+  //     console.log('handleWithdraw', result);
+  //     // await waitForTx(result.hash);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
   // #3 get bank balance
-  useEffect(() => {
-    const init = async () => {
-      if (walletAddress) {
-        await getBalance();
-      }
-    };
-    init();
-  }, [walletAddress]);
+  // useEffect(() => {
+  //   const init = async () => {
+  //     if (walletAddress) {
+  //       await getBalance();
+  //     }
+  //   };
+  //   init();
+  // }, [walletAddress]);
 
   return (
     <div className="h-[90vh] p-4 text-center bg-slate-700">
@@ -103,7 +103,7 @@ const Bank: FC = () => {
       <div className="w-10/12 md:w-9/12 mx-auto my-4 p-4 border border-white bg-gray-100">
         <p className="text-2xl">info</p>
         {/* #3 */}
-        <div className="text-2xl break-all">
+        {/* <div className="text-2xl break-all">
           address : {`${walletAddress || `...`}`}
         </div>
         <div className="text-2xl">
@@ -111,15 +111,15 @@ const Bank: FC = () => {
         </div>
         <div className="text-2xl">
           simple bank contract (balance) : {`${balance || `...`} ETH`}
-        </div>
+        </div> */}
         {/* #4 */}
-        <MyButton id="updateBalanceButton" name="updateBalanceButton" className="mt-4" text="REFRESH" onClick={refreshBalance} />
+        {/* <MyButton id="updateBalanceButton" name="updateBalanceButton" className="mt-4" text="REFRESH" onClick={refreshBalance} /> */}
       </div>
 
       <div className="w-10/12 md:w-9/12 mx-auto my-4 p-4 border border-white bg-gray-100">
         <p className="text-2xl">deposit</p>
         {/* #5 */}
-        <input
+        {/* <input
           name="deposit"
           className="mt-4 p-2 outline-none border text-lg white-glassmorphism rounded-sm"
           type="number"
@@ -127,13 +127,13 @@ const Bank: FC = () => {
           placeholder="input deposit"
           onChange={handleChangeDeposit}
         />
-        <MyButton id="depositButton" name="depositButton" className="mx-2" text="DEPOSIT" onClick={handleDeposit} />
+        <MyButton id="depositButton" name="depositButton" className="mx-2" text="DEPOSIT" onClick={handleDeposit} /> */}
       </div>
 
       <div className="w-10/12 md:w-9/12 mx-auto my-4 p-4 border border-white bg-gray-100">
         <p className="text-2xl">withdraw all bank balance</p>
         {/* #6 */}
-        <MyButton id="withdrawButton" name="withdrawButton" className="mt-4" text="WITHDRAW" onClick={handleWithdraw} />
+        {/* <MyButton id="withdrawButton" name="withdrawButton" className="mt-4" text="WITHDRAW" onClick={handleWithdraw} /> */}
       </div>
 
     </div>
--- dapp/src/pages/Token.tsx
+++ dapp/src/pages/Token.tsx
@@ -9,81 +9,81 @@ const { ethereum } = window;
 
 const Token: FC = () => {
   // #1
-  const { walletAddress, walletBalance, getWalletBalance } = useContext(Web3Context);
-  const [balance, setBalance] = useState("");
-  const [symbol, setSymbol] = useState("");
-  const [deposit, setDeposit] = useState("");
-  const [withdraw, setWithdraw] = useState("");
+  // const { walletAddress, walletBalance, getWalletBalance } = useContext(Web3Context);
+  // const [balance, setBalance] = useState("");
+  // const [symbol, setSymbol] = useState("");
+  // const [deposit, setDeposit] = useState("");
+  // const [withdraw, setWithdraw] = useState("");
 
   // #1
-  const initContract = () => {
-    const provider = new ethers.providers.Web3Provider(ethereum);
-    const signer = provider.getSigner();
-    const tokenContract = new ethers.Contract(TOKEN_ADDRESS, TOKEN_ABI, signer);
-    return tokenContract;
-  };
+  // const initContract = () => {
+  //   const provider = new ethers.providers.Web3Provider(ethereum);
+  //   const signer = provider.getSigner();
+  //   const tokenContract = new ethers.Contract(TOKEN_ADDRESS, TOKEN_ABI, signer);
+  //   return tokenContract;
+  // };
 
   // #2
-  const getSymbol = async () => {
-    const contract = initContract();
-    const result = await contract.symbol();
-    console.log('getSymbol', result);
-    setSymbol(result);
-  };
-  const getBalance = async (): Promise<void> => {
-    const contract = initContract();
-    const result = await contract.balanceOf(walletAddress);
-    // let result = await contract.balances(walletAddress);
-    console.log('getBalance', ethers.utils.formatUnits(result));
-    setBalance(ethers.utils.formatUnits(result));
-  };
+  // const getSymbol = async () => {
+  //   const contract = initContract();
+  //   const result = await contract.symbol();
+  //   console.log('getSymbol', result);
+  //   setSymbol(result);
+  // };
+  // const getBalance = async (): Promise<void> => {
+  //   const contract = initContract();
+  //   const result = await contract.balanceOf(walletAddress);
+  //   // let result = await contract.balances(walletAddress);
+  //   console.log('getBalance', ethers.utils.formatUnits(result));
+  //   setBalance(ethers.utils.formatUnits(result));
+  // };
 
   // #3
-  const handleDeposit = async (): Promise<void> => {
-    try {
-      const tx = {
-        value: ethers.utils.parseEther(deposit)
-      };
-      const contract = initContract();
-      const result = await contract.deposit({ ...tx });
-      console.log('deposit', result);
-    } catch (error) {
-      console.log('handleDeposit error', error);
-    }
-  };
+  // const handleDeposit = async (): Promise<void> => {
+  //   try {
+  //     const tx = {
+  //       value: ethers.utils.parseEther(deposit)
+  //     };
+  //     const contract = initContract();
+  //     const result = await contract.deposit({ ...tx });
+  //     console.log('deposit', result);
+  //   } catch (error) {
+  //     console.log('handleDeposit error', error);
+  //   }
+  // };
 
   // #4
-  const handleWithdraw = async (): Promise<void> => {
-    try {
-      const amount = ethers.utils.parseEther(withdraw);
-      const contract = initContract();
-      const result = await contract.withdraw(amount);
-      console.log('withdraw', result);
-    } catch (error) {
-      console.log('handleWithdraw error', error);
-    }
-  };
+  // const handleWithdraw = async (): Promise<void> => {
+  //   try {
+  //     const amount = ethers.utils.parseEther(withdraw);
+  //     const contract = initContract();
+  //     const result = await contract.withdraw(amount);
+  //     console.log('withdraw', result);
+  //   } catch (error) {
+  //     console.log('handleWithdraw error', error);
+  //   }
+  // };
 
   // #2
-  const handleRefresh = async () => {
-    await Promise.all([
-      getBalance(),
-      getWalletBalance(),
-    ]);
-  };
+  // const handleRefresh = async () => {
+  //   await Promise.all([
+  //     getBalance(),
+  //     getWalletBalance(),
+  //   ]);
+  // };
 
   // #2
-  useEffect(() => {
-    const init = async () => {
-      if (walletAddress) {
-        await Promise.all([
-          getBalance(),
-          getSymbol(),
-        ]);
-      }
-    };
-    init();
-  }, [walletAddress]);
+  // useEffect(() => {
+  //   const init = async () => {
+  //     if (walletAddress) {
+  //       await Promise.all([
+  //         getBalance(),
+  //         getSymbol(),
+  //       ]);
+  //     }
+  //   };
+  //   init();
+  // }, [walletAddress]);
 
   return (
     <div className="h-[90vh] p-4 text-center bg-slate-700">
@@ -91,7 +91,7 @@ const Token: FC = () => {
       <div className="w-10/12 md:w-9/12 mx-auto my-4 p-4 border border-white bg-gray-100 text-2xl">
         <p className="text-2xl">info</p>
         {/* #2 */}
-        <div className="break-all">
+        {/* <div className="break-all">
           address : {`${walletAddress || `...`}`}
         </div>
         <div className="break-all">
@@ -100,12 +100,12 @@ const Token: FC = () => {
         <div className="break-all">
           token balance : {`${balance || `...`} ${symbol}`}
         </div>
-        <MyButton id="updateBalanceButton" name="updateBalanceButton" className="mt-4" text="REFRESH" onClick={handleRefresh} />
+        <MyButton id="updateBalanceButton" name="updateBalanceButton" className="mt-4" text="REFRESH" onClick={handleRefresh} /> */}
       </div>
 
       <div className="w-10/12 md:w-9/12 mx-auto my-4 p-4 border border-white bg-gray-100">
-        <p className="text-2xl">{`Mint ${symbol} from ETH`}</p>
         {/* #3 */}
+        {/* <p className="text-2xl">{`Mint ${symbol} from ETH`}</p>
         <div>
           <input
             name="deposit"
@@ -116,12 +116,12 @@ const Token: FC = () => {
             onChange={e => setDeposit(e.target.value)}
           />
           <MyButton id="depositButton" name="depositButton" className="mx-2" text="MINT" onClick={handleDeposit} />
-        </div>
+        </div> */}
       </div>
 
       <div className="w-10/12 md:w-9/12 mx-auto my-4 p-4 border border-white bg-gray-100">
-        <p className="text-2xl">{`Convert ${symbol} back to ETH`}</p>
         {/* #4 */}
+        {/* <p className="text-2xl">{`Convert ${symbol} back to ETH`}</p>
         <div>
           <input
             name="withdraw"
@@ -132,7 +132,7 @@ const Token: FC = () => {
             onChange={e => setWithdraw(e.target.value)}
           />
           <MyButton id="withdrawButton" name="withdrawButton" className="mx-2" text="CONVERT" onClick={handleWithdraw} />
-        </div>
+        </div> */}
       </div>
 
     </div>
